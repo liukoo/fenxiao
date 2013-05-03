00e359dd697b2c2f5641ec3acf0f5dd97522f5cd
liukoo <lqf800@qq.com>
2013-05-03 17:29:55 +0800

    修改

--- fenxiao/agent/views.py
+++ fenxiao/agent/views.py
@@ -4,14 +4,19 @@ from  django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 import fenxiao.settings
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 def index(request):
-            user_cache = authenticate(username='admin', password='admin')
-            #如果成功返回对应的User对象，否则返回None(只是判断此用户是否存在，不判断是否is_active或者is_staff)
-            if user_cache is None:
-                msg ="您输入的用户名或密码不正确!"
-            elif not user_cache.is_active or not user_cache.is_staff:
-                msg ="您输入的用户名或密码不正确!"
-            else:
-                login(request,user_cache)
-            return HttpResponse(msg)
+     msg =""
+     user_cache = authenticate(username='admin', password='admin')
+     #如果成功返回对应的User对象，否则返回None(只是判断此用户是否存在，不判断是否is_active或者is_staff)
+     if user_cache is None:
+        msg ="您输入的用户名或密码不正确!"
+     elif not user_cache.is_superuser:
+            msg ="您不是管理员!"
+     else:
+            login(request,user_cache)
+     return HttpResponse(msg)
+def add(request):
+    msg = User.objects.create(username='liukoo',password='liukoo')
+    return HttpResponse(msg)
 
--- fenxiao/urls.py
+++ fenxiao/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^add/$', 'fenxiao.agent.views.add'),
     (r'^$', 'fenxiao.agent.views.index'),
 )
 urlpatterns += staticfiles_urlpatterns()
